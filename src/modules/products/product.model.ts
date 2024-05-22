2c5750e4542386541753d08d1ff044b2aa5d0ee5
rubina23 <rubinacse14@gmail.com>
2024-05-22 18:32:59 +0600

    crud operation done for order

--- src/modules/products/product.model.ts
+++ src/modules/products/product.model.ts
@@ -4,42 +4,52 @@ import { TInventory, TProduct, Tvariants } from "./product.interface";
 const variantSchema = new Schema<Tvariants>({
   type: {
     type: String,
+    required: true,
   },
   value: {
     type: String,
+    required: true,
   },
 });
 
 const inventorySchema = new Schema<TInventory>({
   quantity: {
     type: Number,
+    required: true,
   },
   inStock: {
     type: Boolean,
+    required: true,
   },
 });
 
 const productSchema = new Schema<TProduct>({
   name: {
     type: String,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   price: {
     type: Number,
+    required: true,
   },
   category: {
     type: String,
+    required: true,
   },
   tags: {
     type: [String],
   },
   variants: {
     type: [variantSchema],
+    required: true,
   },
   inventory: {
     type: inventorySchema,
+    required: true,
   },
 });
 
